ead22ad16a25231d28c8efc4541305a894750746
David Araujo <dlfaraujo@gmail.com>
2021-05-07 17:04:39 -0700

    Added json and proto examples

---
+++ src/main/java/io/confluent/demo/aircraft/protobuf/producer/AircraftEvent.java
@@ -0,0 +1,63 @@
+package io.confluent.demo.aircraft.jsonschema.producer;
+
+import io.confluent.demo.aircraft.jsonschema.pojo.AircraftState;
+import org.opensky.model.StateVector;
+
+public class AircraftEvent {
+    
+    public static AircraftState create(StateVector state) {
+
+        AircraftState event = new AircraftState();
+        
+        if (state.getBaroAltitude() != null)
+            event.setBaroAltitude(state.getBaroAltitude());
+
+        if (state.getCallsign() != null)
+            event.setCallsign(state.getCallsign().trim());
+        else
+            event.setCallsign("");
+
+        if (state.getGeoAltitude() != null)
+            event.setGeoAltitude(state.getGeoAltitude());
+
+        if (state.getBaroAltitude() != null)
+            event.setBaroAltitude(state.getBaroAltitude());
+
+        if (state.getIcao24() != null)
+            event.setIcao24(state.getIcao24());
+
+        if (state.getLastContact() != null)
+            event.setLastContact(state.getLastContact().intValue());
+
+        if (state.getLatitude() != null)
+            event.setLatitude(state.getLatitude());
+
+        if (state.getLongitude() != null)
+            event.setLongitude(state.getLongitude());
+        //event.setOnGround(state.isOnGround());
+
+        if (state.getOriginCountry() != null)
+            event.setCountryOrigin(state.getOriginCountry());
+
+        event.setSpi(state.isSpi());
+
+        if (state.getSquawk() != null)
+            event.setSquawk(state.getSquawk());
+
+        if (state.getLastPositionUpdate() != null)
+            event.setTimePosition(state.getLastPositionUpdate());
+
+        if (state.getHeading() != null)
+            event.setTrueTrack(state.getHeading());
+
+        if (state.getVelocity() != null)
+            event.setVelocity(state.getVelocity());
+
+        if (state.getVerticalRate() != null)
+            event.setVerticalRate(state.getVerticalRate());
+
+        event.setOnGround(state.isOnGround());
+        
+        return event;
+    }
+}
